b42e3cecd75f8c9040614bfd66bf8773349fd017
Team3773 <77553727+Team3773@users.noreply.github.com>
2025-01-29 20:19:27 -0500

    Added additional subsystems

---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.RelativeEncoder;
+
+public class ArmSubsystem {
+//Neo Vortex Spark FLEX with 67:1 Gear Ratio    
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsytem.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+
+public class ShooterSubsytem {
+//Neo 550 with Spark MAX and 3:1 Gear Ratio    
+}
