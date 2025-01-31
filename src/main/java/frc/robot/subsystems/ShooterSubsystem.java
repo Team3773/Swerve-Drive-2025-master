8ee920537327eb08b11d84873201a75509f02484
Team3773 <77553727+Team3773@users.noreply.github.com>
2025-01-30 19:59:47 -0500

    Created Imports

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterSubsystem {
-//Neo 550 with Spark MAX and 3:1 Gear Ratio    
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.REVLibError;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
+import edu.wpi.first.hal.SimDouble;
+import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
+
+//NEO 550 with Spark MAX and 3:1 Gear Ratio
+
+/** Creates new ShooterSubsystem. */
+public class ShooterSubsystem extends SubsystemBase {
+    
 }
