97c9db630d1fd1b27321e15d7bbec63f9805104d
phobos42 <garrettruffner42@gmail.com>
2025-12-02 20:53:19 -0600

    Introduce Profiled PID Controller

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,22 +4,19 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -27,53 +24,48 @@ import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
+    // Limits
+    private static double kDt = 0.02;
+    private static double kMaxVelocity = 1.75;
+    private static double kMaxAcceleration = 0.75;
+    private static double kP = 1.3;
+    private static double kI = 0.0;
+    private static double kD = 0.7;
+    private static double kS = 1.1;
+    private static double kG = 1.2;
+    private static double kV = 1.3;
+
+    private final TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(kMaxVelocity,
+            kMaxAcceleration);
+    private final ProfiledPIDController pidController = new ProfiledPIDController(kP, kI, kD, m_constraints, kDt);
+    private final ElevatorFeedforward m_feedforward = new ElevatorFeedforward(kS, kG, kV);
+
     private SparkMax leftMotor;
     private SparkMax rightMotor;
     private CANcoder CANcoder;
-    private final SparkClosedLoopController leftClosedLoopController;
-    // private final SparkClosedLoopController rightClosedLoopController;
-    // private final RelativeEncoder absoluteEncoder;
-    // private final SparkAbsoluteEncoder absoluteEncoder;
-    // private final RelativeEncoder rightEncoder;
     private DigitalInput resetlimitSwitch;
-    // private DigitalInput toplimitSwitch;
-
     private double currentSetPoint = 0;
 
     /** Creates a new ElevatorSubsystem. */
-    public ElevatorSubsystem() {  
+    public ElevatorSubsystem() {
         leftMotor = new SparkMax(Constants.ElevatorConstants.LEFT_CAN_ID, MotorType.kBrushless);
         rightMotor = new SparkMax(Constants.ElevatorConstants.RIGHT_CAN_ID, MotorType.kBrushless);
         CANcoder = new CANcoder(Constants.ElevatorConstants.CANcoder_ID);
-        
-        // rightClosedLoopController = rightMotor.getClosedLoopController();
-        // leftEncoder = leftMotor.getEncoder();
-        
-        // rightEncoder = rightMotor.getEncoder();
+
+        var toApply = new CANcoderConfiguration();
+        /*
+         * User can change the configs if they want, or leave it empty for
+         * factory-default
+         */
+        CANcoder.getConfigurator().apply(toApply);
+        /* Speed up signals to an appropriate rate */
+        BaseStatusSignal.setUpdateFrequencyForAll(100, CANcoder.getPosition(), CANcoder.getVelocity());
 
         SparkMaxConfig globalConfig = new SparkMaxConfig();
         SparkMaxConfig leaderConfig = new SparkMaxConfig();
         SparkMaxConfig followerConfig = new SparkMaxConfig();
 
         resetlimitSwitch = new DigitalInput(Constants.ElevatorConstants.RESET_LIMIT_PORT);
-        // toplimitSwitch = new DigitalInput(Constants.ElevatorConstants.TOP_LIMIT_PORT);
-
-        leaderConfig.absoluteEncoder
-                .positionConversionFactor(1)
-                .velocityConversionFactor(1)
-                .zeroOffset(Constants.ElevatorConstants.AbsoluteEncoderOffset);
-
-        leaderConfig.closedLoop
-                .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
-                .p(1.0)
-                .i(1e-4)
-                .d(1)
-                .outputRange(-1, 1)
-                .p(0.1, ClosedLoopSlot.kSlot1)
-                .i(0, ClosedLoopSlot.kSlot1)
-                .d(0, ClosedLoopSlot.kSlot1)
-                .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
-                .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
 
         globalConfig
                 .smartCurrentLimit(50)
@@ -86,31 +78,20 @@ public class ElevatorSubsystem extends SubsystemBase {
         rightMotor.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
         // Initialize dashboard values
-        SmartDashboard.setDefaultNumber("Elevator Target Position", 0);
-        SmartDashboard.setDefaultNumber("Elevator Target Velocity", 0);
-        
-        //Assume position is below the limit switch on startup
-        // leftEncoder.setPosition(0);
-        leftClosedLoopController = leftMotor.getClosedLoopController();
+        SmartDashboard.setDefaultNumber("Elevator setPoint", 0);
+        SmartDashboard.setDefaultNumber("Elevator Encoder Position", -1);
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
         SmartDashboard.putNumber("Elevator setPoint", currentSetPoint);
-        SmartDashboard.putNumber("Elevator Left Encoder Position", getPostion());
-        // SmartDashboard.putNumber("Elevator Right Encoder Position",
-        // rightEncoder.getPosition());
-        SmartDashboard.putNumber("Elevator Left Velocity", getPostion());
-        // SmartDashboard.putNumber("Elevator Right Velocity",
-        // rightEncoder.getVelocity());
+        SmartDashboard.putNumber("Elevator Encoder Position", getPostion());
 
         // Check the limit switch and reset the encoder if it is pressed
         if (isResetLimitSwitchPressed()) {
-            // resetEncoder();
-            if(this.currentSetPoint <= this.getPostion()){
-              this.stop();
-          }
+             resetEncoder();
+             goToPosition(0);
         }
     }
 
@@ -120,17 +101,17 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     // public boolean isTopLimitSwitchPressed() {
-    //     return toplimitSwitch.get(); // Assuming it's normally closed
+    // return toplimitSwitch.get(); // Assuming it's normally closed
     // }
 
-    // public void resetEncoder() {
-    //     leftEncoder.setPosition(0);
-    // }
+    public void resetEncoder() {
+        CANcoder.setPosition(0);
+    }
 
     public void incrementPosition() {
         // if (isTopLimitSwitchPressed()) {
-        //     stop(); // Prevent further movement
-        //     return;
+        // stop(); // Prevent further movement
+        // return;
         // }
         currentSetPoint += Constants.ElevatorConstants.stepValue;
         goToPosition(currentSetPoint);
@@ -142,12 +123,16 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public void goToPosition(double value) {
+
+        pidController.setGoal(value);
         currentSetPoint = value;
-        leftClosedLoopController.setReference(value, ControlType.kPosition, ClosedLoopSlot.kSlot1);
+        leftMotor.setVoltage(
+                pidController.calculate(getPostion())
+                        + m_feedforward.calculate(pidController.getSetpoint().velocity));
     }
 
-    public double getPostion(){
-        return CANcoder.getAbsolutePosition().getValueAsDouble();
+    public double getPostion() {
+        return CANcoder.getPosition().getValueAsDouble();
     }
 
     public void stop() {
